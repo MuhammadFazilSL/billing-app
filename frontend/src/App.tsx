e454f6c0d43b8ac1d4ddda483e4c7e2d480568db
muhammad_fazil <muhammadfazilsl455@gmail.com>
2026-07-16 17:25:15 +0530

    sprint6 error fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { store } from './store/store';
 import { AppRoutes } from './routes';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </QueryClientProvider>
     </Provider>
   );
 }
